630c8e42df3e74894e3c67070c0e37021a328b89
Alexandra PETITJEAN <apetitje@e3r12p15.42.fr>
2016-11-04 13:25:09 +0100

    ft_isalpha

--- libft.h
+++ libft.h
@@ -6,12 +6,14 @@
 /*   By: apetitje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 12:18:37 by apetitje          #+#    #+#             */
-/*   Updated: 2016/11/04 12:48:21 by apetitje         ###   ########.fr       */
+/*   Updated: 2016/11/04 13:19:06 by apetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <stddef.h>
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
