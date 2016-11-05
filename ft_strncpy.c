0c7c308e972e2a78da94b07bf520f3c6bbe72692
Alexandra PETITJEAN <apetitje@e2r11p2.42.fr>
2016-11-05 21:26:15 +0100

    strncpy ok

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: apetitje <apetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 14:33:46 by apetitje          #+#    #+#             */
-/*   Updated: 2016/11/05 20:14:04 by apetitje         ###   ########.fr       */
+/*   Updated: 2016/11/05 21:23:37 by apetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t	i;
 
 	i = 0;
+	if (len == 0)
+		return (NULL);
+	while (src[i] && i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
 	while (i < len)
 	{
-		while (src[i] && i < len)
-		{
-			dst[i] = src[i];
-			i++;
-		}
 		dst[i] = '\0';
 		i++;
 	}
